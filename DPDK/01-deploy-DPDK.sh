07f2d3fa0475efa684dacdb66e0e75f63ae1618e
“[Mohammed <[m.elsayed06@gmail.com]>
2017-12-26 09:04:11 +0400

    first commit

--- DPDK/01-deploy-DPDK.sh
+++ DPDK/01-deploy-DPDK.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-openstack overcloud deploy --templates \
--r ./template/roles_data.yaml \
--e ./template/node-info.yaml \
--e ./template/network-isolation.yaml \
--e ./template/network-environment.yaml \
--e ./template/storage-environment.yaml \
--e ./template/timezone-environment.yaml \
+openstack overcloud deploy --templates /home/stack/DPDK/openstack-tripleo-heat-templates \
+-r ./templates/roles_data.yaml \
+-e ./templates/node-info.yaml \
+-e ./templates/network-isolation.yaml \
+-e ./templates/network-environment.yaml \
+-e ./templates/storage-environment.yaml \
+-e ./templates/timezone-environment.yaml \
 --stack overcloud  $1 | tee  ./openstack-deployment.log
