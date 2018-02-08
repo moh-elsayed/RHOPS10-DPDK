ca5eafde8c016e9a0bf2d64ddcb348abb0427bf5
“[Mohammed <[m.elsayed06@gmail.com]>
2018-02-08 19:13:59 +0400

    adding the DPDK

---
+++ DONE/No-OVS/DPDK/01-deploy-DPDK.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+openstack overcloud deploy --templates /home/stack/DPDK/openstack-tripleo-heat-templates \
+-r ./templates/roles_data.yaml \
+-e ./templates/node-info.yaml \
+-e ./templates/network-isolation.yaml \
+-e ./templates/network-environment.yaml \
+-e ./templates/storage-environment.yaml \
+-e ./templates/timezone-environment.yaml \
+--stack overcloud  $1 | tee  ./openstack-deployment.log
